07296a01dd2ee9fc1ae8828c9cac21339f2f262b
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2025-04-06 13:33:05 -0400

    fix: empty list return values

--- lib/mix/tasks/calculate_metrics.ex
+++ lib/mix/tasks/calculate_metrics.ex
@@ -15,34 +15,7 @@ defmodule Mix.Tasks.CalculateMetrics do
               System.halt(1)
             end
 
-            followers = Enum.map(users_data, fn user -> user["followers_count"] end)
-            followings = Enum.map(users_data, fn user -> user["following_count"] end)
-
-            account_ages =
-              users_data
-              |> Enum.map(fn user ->
-                case Statistics.account_age_in_days(user["created_at"]) do
-                  {:ok, age} ->
-                    age
-
-                  {:error, reason} ->
-                    IO.puts("Error calculating account age: #{inspect(reason)}")
-                    nil
-                end
-              end)
-              |> Enum.filter(fn age -> is_number(age) end)
-
-            metrics = [
-              {"Followers", Statistics.min(followers), Statistics.max(followers),
-               Statistics.average(followers), Statistics.median(followers),
-               Statistics.standard_deviation(followers)},
-              {"Following", Statistics.min(followings), Statistics.max(followings),
-               Statistics.average(followings), Statistics.median(followings),
-               Statistics.standard_deviation(followings)},
-              {"Account Age", Statistics.min(account_ages), Statistics.max(account_ages),
-               Statistics.average(account_ages), Statistics.median(account_ages),
-               Statistics.standard_deviation(account_ages)}
-            ]
+            metrics = Statistics.generate_metrics(users_data)
 
             csv_data =
               ["Metric, min, max, avg, median, std\n"] ++
--- lib/statistics.ex
+++ lib/statistics.ex
@@ -23,16 +23,16 @@ defmodule Statistics do
     end
   end
 
-  def max([]), do: 0
+  def max([]), do: nil
   def max(values), do: Enum.max(values)
 
-  def min([]), do: 0
+  def min([]), do: nil
   def min(values), do: Enum.min(values)
 
-  def average([]), do: 0.0
+  def average([]), do: nil
   def average(values), do: Enum.sum(values) / length(values)
 
-  def median([]), do: 0.0
+  def median([]), do: nil
 
   def median(values) do
     sorted_values = Enum.sort(values)
@@ -47,7 +47,7 @@ defmodule Statistics do
     end
   end
 
-  def standard_deviation([]), do: 0.0
+  def standard_deviation([]), do: nil
 
   def standard_deviation(values) do
     mean = average(values)
@@ -65,5 +65,36 @@ defmodule Statistics do
     |> Enum.map(&String.downcase/1)
     |> Enum.frequencies()
     |> Enum.sort_by(fn {location, freq} -> {-freq, location} end)
+  end
+
+  def generate_metrics(values) do
+    followers = Enum.map(values, fn user -> user["followers_count"] end)
+    followings = Enum.map(values, fn user -> user["following_count"] end)
+
+    account_ages =
+      values
+      |> Enum.map(fn user ->
+        case Statistics.account_age_in_days(user["created_at"]) do
+          {:ok, age} ->
+            age
+
+          {:error, reason} ->
+            IO.puts("Error calculating account age: #{inspect(reason)}")
+            nil
+        end
+      end)
+      |> Enum.filter(fn age -> is_number(age) end)
+
+    [
+      {"Followers", Statistics.min(followers), Statistics.max(followers),
+       Statistics.average(followers), Statistics.median(followers),
+       Statistics.standard_deviation(followers)},
+      {"Following", Statistics.min(followings), Statistics.max(followings),
+       Statistics.average(followings), Statistics.median(followings),
+       Statistics.standard_deviation(followings)},
+      {"Account Age", Statistics.min(account_ages), Statistics.max(account_ages),
+       Statistics.average(account_ages), Statistics.median(account_ages),
+       Statistics.standard_deviation(account_ages)}
+    ]
   end
 end
